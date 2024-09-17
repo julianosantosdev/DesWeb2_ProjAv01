48047542c3173d78ad531ca26093f53b98cb4341
Juliano Santos <julianosantosdev@outlook.com>
2024-09-17 15:46:49 -0300

    chore: refactor

--- checkCredentials.php
+++ checkCredentials.php
@@ -1,5 +1,5 @@
 <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST" || $_SERVER["REQUEST_METHOD"] == "GET"){
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
         session_start();
         if($_POST['user'] == 'coordenacao' && $_POST['password'] == 'coordenacao' || $_POST['user'] == 'tecnicos' && $_POST['password'] == 'tecnicos'){
             $_SESSION['logged'] = TRUE;
